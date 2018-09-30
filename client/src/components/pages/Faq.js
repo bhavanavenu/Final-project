6c0869d40e540c2cc96e0cf6d53b7fee465ed8dc
Bhavana <bhavanavenugopal2@gmail.com>
2018-09-30 13:18:57 +0200

    add favicon

--- client/src/components/pages/Faq.js
+++ client/src/components/pages/Faq.js
@@ -1,8 +1,3 @@
-// import React, { Component } from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
-// import "./Faq.css";
-
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -12,38 +7,38 @@ class Faq extends Component {
   render() {
     return (
       <div className="container ">
-        <div className="faq align-middle mt-5 shadow-lg p-5 rounded">
-          <dl>
-            <dt style={{ fontSize: 20 }}>What is Cipher?</dt>
-            <dd>
-              If you need to send a password or some other form of simple but
-              sensitive information to someone you can not send it over IM or
-              email. These methods are not secure as anyone with little
-              knowledge can intercept this information during transmission.
-              Using Cipher as the "middle man" you can safely and securely
-              transfer this data to your recipient.
-            </dd>
-            <dt style={{ fontSize: 20 }}>How secure is it?</dt>
-            <dd>
-              Cipher runs on HTTPS. Once the data is accessed by the receiver,
-              the encrypted documents on the backend are removed from the
-              system.
-            </dd>
-            <dt style={{ fontSize: 20 }}>
-              How are the documents stored before being read?
-            </dt>
-            <dd>The documents are encrypted and put into a database.</dd>
-            <dt style={{ fontSize: 20 }}>Can you read the notes?</dt>
-            <dd>
-              No. The notes are encrypted using a key that is never stored on
-              the server. Only the valid URL can display the documents. Once
-              they are viewed the encrypted documents are removed from the
-              system and the link can not be accessed again.
-            </dd>
-          </dl>
+        <div className="faq align-middle mt-5 shadow-lg p-3 rounded">
+          <h2 style={{ fontSize: 26 }}>What is Cipher?</h2>
 
           <p>
-            <h5>About Author</h5>
+            If you need to send a password or some other form of simple but
+            sensitive information to someone you can not send it over IM or
+            email. These methods are not secure as anyone with little knowledge
+            can intercept this information during transmission. Using Cipher as
+            the "middle man" you can safely and securely transfer this data to
+            your recipient.
+          </p>
+
+          <h2 style={{ fontSize: 26 }}>How secure is it?</h2>
+          <p>
+            Cipher runs on HTTPS. Once the data is accessed by the receiver, the
+            encrypted documents on the backend are removed from the system.
+          </p>
+
+          <h2 style={{ fontSize: 26 }}>
+            How are the documents stored before being read?
+          </h2>
+          <p>The documents are encrypted and put into a database.</p>
+          <h2 style={{ fontSize: 26 }}>Can you read the notes?</h2>
+          <p>
+            No. The notes are encrypted using a key that is never stored on the
+            server. Only the valid URL can display the documents. Once they are
+            viewed the encrypted documents are removed from the system and the
+            link can not be accessed again.
+          </p>
+
+          <h2 style={{ fontSize: 26 }}>About Author</h2>
+          <p>
             Hi, my name is Bhavana Venu, I developed this project for helping
             anyone to share information anonymously to anyone in a secure way. I
             am activly improving this application and you can checkout the
